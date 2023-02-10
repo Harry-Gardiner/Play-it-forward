d50defbfc06400f5feade18e46ec34833e4b7e00
Ellie Hobbs <eshobbs7@gmail.com>
2023-02-10 15:17:59 +0000

    Added splitter scss file for later, reordered css utilities files, got flow to work

--- web/app/themes/sage/resources/views/blocks/cta-banner.blade.php
+++ web/app/themes/sage/resources/views/blocks/cta-banner.blade.php
@@ -1,5 +1,6 @@
 <div class="{{ $block->classes }}">
-  <div>
+  <div class="cta-banner flow">
     <h1>I am a CTA banner</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi odit dignissimos earum. Deleniti, libero eius eligendi, asperiores ratione consectetur dolor est repellendus soluta consequatur harum ullam. Dolores, nihil corporis. Obcaecati.</p>
   </div>
 </div>
