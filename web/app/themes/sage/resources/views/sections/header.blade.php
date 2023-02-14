3c4a014e52839d08d616aa5891d13f5f0d177542
Harry Gardiner Lunar <harry@lunar.build>
2023-02-14 09:49:36 +0100

    Added white logo svg. Started work on header and mobile main nav.

--- web/app/themes/sage/resources/views/sections/header.blade.php
+++ web/app/themes/sage/resources/views/sections/header.blade.php
@@ -1,11 +1,22 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
+<header class="header full-bleed">
+  <div class="header__wrapper">
+    <a class="logo" href="{{ home_url('/') }}">
+      <div class="logo__wrapper">
+        <img src="{{ asset('images/logo_white.svg') }}" alt="Play it forward logo">
+        <p>play it <strong>forward</strong></p>
+      </div>
+    </a>
+  
+    {{-- Hamburger Icon --}}
+    <input class="side-menu" type="checkbox" id="side-menu"/>
+    <label class="hamb" for="side-menu"><span class="hamb-line"></span></label> 
+ </div> 
 
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+
+    {{-- Primary nav --}}
+    @if (has_nav_menu('primary_navigation'))
+      <nav class="nav" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu container', 'echo' => false]) !!}
+      </nav>
+    @endif
 </header>
