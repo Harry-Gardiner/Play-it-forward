f16f25ce49812ad3e7f1b27a7bf26cda28b3385a
Harry Gardiner Lunar <harry@lunar.build>
2023-02-15 10:02:49 +0100

    Further progress made. Nav is now full width

--- web/app/themes/sage/resources/views/sections/header.blade.php
+++ web/app/themes/sage/resources/views/sections/header.blade.php
@@ -8,14 +8,13 @@
         </a>
 
         {{-- Hamburger Icon --}}
-        <input class="drop-menu" type="checkbox" id="side-menu" />
         <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
-
-        {{-- Primary nav --}}
-        @if (has_nav_menu('primary_navigation'))
-            <nav class="nav" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-                {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu container', 'echo' => false]) !!}
-            </nav>
-        @endif
     </div>
+    <input class="drop-menu" type="checkbox" id="side-menu" />
+    {{-- Primary nav --}}
+    @if (has_nav_menu('primary_navigation'))
+        <nav class="nav" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu', 'echo' => false]) !!}
+        </nav>
+    @endif
 </header>
