a8e5ddd36376d25aea89dddac2b8c3971df91114
Harry Gardiner Lunar <harry@lunar.build>
2023-02-16 08:14:20 +0100

    reworked nav so its responsive with md media query.

--- web/app/themes/sage/resources/views/sections/header.blade.php
+++ web/app/themes/sage/resources/views/sections/header.blade.php
@@ -1,5 +1,5 @@
 <header class="header full-bleed">
-    <div class="header__wrapper container">
+    <div class="header__wrapper">
         <a class="logo" href="{{ home_url('/') }}">
             <div class="logo__wrapper">
                 <img src="{{ asset('images/logo_white.svg') }}" alt="Play it forward logo">
@@ -9,12 +9,13 @@
 
         {{-- Hamburger Icon --}}
         <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+        <input class="drop-menu" type="checkbox" id="side-menu" />
+        
+        {{-- Primary nav --}}
+        @if (has_nav_menu('primary_navigation'))
+            <nav class="nav" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+                {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu', 'echo' => false]) !!}
+            </nav>
+        @endif
     </div>
-    <input class="drop-menu" type="checkbox" id="side-menu" />
-    {{-- Primary nav --}}
-    @if (has_nav_menu('primary_navigation'))
-        <nav class="nav" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu', 'echo' => false]) !!}
-        </nav>
-    @endif
 </header>
