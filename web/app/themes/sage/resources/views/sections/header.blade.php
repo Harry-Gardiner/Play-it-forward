0308005a395e51a11fab0bd0c26e0a1213ae6bd8
Harry Gardiner Lunar <harry@lunar.build>
2023-02-24 17:16:42 +0100

    Initial desktop functionality sorted

--- web/app/themes/sage/resources/views/sections/header.blade.php
+++ web/app/themes/sage/resources/views/sections/header.blade.php
@@ -2,7 +2,7 @@
     <div class="header__wrapper">
         <a class="logo" href="{{ home_url('/') }}">
             <img src="{{ asset('images/logo_white.svg') }}" alt="Play it forward logo">
-            <p>play it <strong>forward</strong></p>
+            <p>play it <span class="strong">forward</span></p>
         </a>
     
         <input class="drop-menu" type="checkbox" id="drop-menu" />
