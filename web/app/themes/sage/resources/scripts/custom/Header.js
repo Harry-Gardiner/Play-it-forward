6d805536bee60d1cd451725e860bf6e6e6375b4e
Harry Gardiner Lunar <harry@lunar.build>
2023-02-28 08:34:55 +0100

    Added aria expanded attribute to nav elements

--- web/app/themes/sage/resources/scripts/custom/Header.js
+++ web/app/themes/sage/resources/scripts/custom/Header.js
@@ -1,6 +1,6 @@
 /**
  * Sub Nav toggle
- * 
+ *
  * The mobile menu toggle is controlled using a checkbox and css, no JS applied.
  * Subnav logic open and close is controlled below.
  */
@@ -11,11 +11,31 @@ const navItems = document.querySelectorAll('.menu-item a');
 
 function toggleSubnav(e) {
   for (var i = 0; i < dropdownBtns.length; i++) {
+    const visuallyHiddenText = dropdownBtns[i].firstElementChild;
+    const subnav = dropdownBtns[i].nextElementSibling;
+
     if (dropdownBtns[i] == e.target) {
       dropdownBtns[i].classList.toggle('active');
-      dropdownBtns[i].nextElementSibling.classList.toggle('open');
+
+      if (dropdownBtns[i].classList.contains('active')) {
+        visuallyHiddenText.innerHTML = 'Click to close the subnav';
+        // dropdownBtns[i].setAttribute('aria-controls', '')
+      } else {
+        visuallyHiddenText.innerHTML = 'Click to access dropdown menu';
+        // dropdownBtns[i].setAttribute('aria-controls', false)
+      }
+
+      subnav.classList.toggle('open');
+
+      if (subnav.classList.contains('open')) {
+        subnav.setAttribute('aria-expanded', true)
+      } else {
+        subnav.setAttribute('aria-expanded', false)
+      }
+
     } else {
       dropdownBtns[i].classList.remove('active');
+      visuallyHiddenText.innerHTML = 'Click to access dropdown menu';
       dropdownBtns[i].nextElementSibling.classList.remove('open');
     }
   }
@@ -30,23 +50,23 @@ dropdownBtns.forEach((button) => {
 });
 
 // Close all subnavs if open
-function closeNavs(){
-  dropdownBtns.forEach(button => {
+function closeNavs() {
+  dropdownBtns.forEach((button) => {
     button.classList.remove('active');
-  })
-  dropdownMenus.forEach(menu => {
+  });
+  dropdownMenus.forEach((menu) => {
     menu.classList.remove('open');
   });
 }
 
 // Handles moving out of the nav
-navItems.forEach(item => {
-  item.addEventListener('click', function (e){
-    closeNavs()
-  })
+navItems.forEach((item) => {
+  item.addEventListener('click', function (e) {
+    closeNavs();
+  });
 });
 
 // Handle closing subnav if clicking outside of nav
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
   closeNavs();
 });
