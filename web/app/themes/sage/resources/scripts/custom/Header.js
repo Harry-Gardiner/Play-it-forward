a5d7f1a40aa145f0abfad1d9888acb0880802dab
Harry Gardiner Lunar <harry@lunar.build>
2023-04-14 15:21:14 +0100

    Logic added to open and close sub navs with tab click.

--- web/app/themes/sage/resources/scripts/custom/Header.js
+++ web/app/themes/sage/resources/scripts/custom/Header.js
@@ -3,33 +3,34 @@ var menuItems = document.querySelectorAll('li.menu-item-has-children');
 Array.prototype.forEach.call(menuItems, function (el) {
   el.addEventListener('mouseover', function () {
     this.classList.add('open');
-    this.querySelector('.caret').setAttribute('aria-expanded', 'true');
+    this.querySelector('.sub-menu-btn').setAttribute('aria-expanded', 'true');
   });
   el.addEventListener('mouseleave', function () {
-    setTimeout(function () {
-      el.classList.remove('open');
-      el.querySelector('.caret').setAttribute('aria-expanded', 'false');
-    }, 300);
+    el.classList.remove('open');
+    el.querySelector('.sub-menu-btn').setAttribute('aria-expanded', 'false');
   });
 });
 
 Array.prototype.forEach.call(menuItems, function (el) {
   var activatingA = el.querySelector('a');
 
-  var btn =
-    '<button class="caret" aria-haspopup="true" aria-expanded="false" tabindex="0"><span class="visually-hidden">show submenu for ' +
-    activatingA.text +
-    '</span></button>';
-  activatingA.insertAdjacentHTML('afterend', btn);
+  function menuToggle(event) {
+    if (!this.parentNode.classList.contains('open')) {
+      var elems = document.querySelectorAll('.open');
+      [].forEach.call(elems, function (el) {
+        el.classList.remove('open');
+        // console.log(el.childNodes);
+        el.querySelector('button').setAttribute('aria-expanded', 'false');
+        el.querySelector('a').setAttribute('aria-expanded', 'false');
+      });
 
-  el.querySelector('button').addEventListener('click', function (event) {
-    if (!this.parentNode.classList.contains('open'))  {
       this.parentNode.classList.add('open');
       this.parentNode.querySelector('a').setAttribute('aria-expanded', 'true');
       this.parentNode
         .querySelector('button')
         .setAttribute('aria-expanded', 'true');
-    } else {
+    }
+    else {
       this.parentNode.classList.remove('open');
       this.parentNode.querySelector('a').setAttribute('aria-expanded', 'false');
       this.parentNode
@@ -37,5 +38,14 @@ Array.prototype.forEach.call(menuItems, function (el) {
         .setAttribute('aria-expanded', 'false');
     }
     event.preventDefault();
-  });
+  }
+
+  var btn =
+    '<button class="sub-menu-btn" aria-haspopup="true" aria-expanded="false" tabindex="0"><span class="caret"></span><span class="visually-hidden">show submenu for ' +
+    activatingA.text +
+    '</span></button>';
+
+  activatingA.insertAdjacentHTML('afterend', btn);
+
+  el.querySelector('button').addEventListener('click', menuToggle);
 });
