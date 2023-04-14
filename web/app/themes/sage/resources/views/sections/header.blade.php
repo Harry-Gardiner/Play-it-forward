5e3b0b0df093f9b82d77f84a525dbe4e40a927f3
Harry Gardiner Lunar <harry@lunar.build>
2023-04-14 15:34:28 +0100

    Added logo and styled

--- web/app/themes/sage/resources/views/sections/header.blade.php
+++ web/app/themes/sage/resources/views/sections/header.blade.php
@@ -16,15 +16,23 @@
     </div>  
 </header> --}}
 
-<header class="full-bleed">
-    @if (has_nav_menu('primary_navigation'))
-        <nav class="nav__desktop container" role="navigation" aria-label="<?php _e('Main Menu', 'textdomain'); ?>">
-            {!! wp_nav_menu([
-                'theme_location' => 'primary_navigation',
-                'container' => false,
-                'menu_class' => 'main-navigation',
-                'items_wrap' => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>',
-            ]) !!}
-        </nav>
-    @endif
+<header class="header full-bleed">
+    <div class="header__wrapper container">
+        <a class="logo" href="{{ home_url('/') }}">
+            <div class="logo__wrapper">
+                <img src="{{ asset('images/logo_white.svg') }}" alt="Play it forward logo">
+                <p>play it <span class="strong">forward</span></p>
+            </div>
+        </a>
+        @if (has_nav_menu('primary_navigation'))
+            <nav class="nav__desktop container" role="navigation" aria-label="<?php _e('Main Menu', 'textdomain'); ?>">
+                {!! wp_nav_menu([
+                    'theme_location' => 'primary_navigation',
+                    'container' => false,
+                    'menu_class' => 'main-navigation',
+                    'items_wrap' => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>',
+                ]) !!}
+            </nav>
+        @endif
+    </div>
 </header>
