6a7d2fff83463fed3cc9be5bacd365f72d853a74
Harry Gardiner Lunar <harry@lunar.build>
2023-06-25 22:05:38 +0100

    JSON contents passed to JS ACF colour picker args

--- web/app/themes/sage/app/setup.php
+++ web/app/themes/sage/app/setup.php
@@ -7,7 +7,6 @@
 namespace App;
 
 use function Roots\bundle;
-require_once('helper.php');
 
 /**
  * Register the theme assets.
@@ -181,20 +180,45 @@ class AWP_Menu_Walker extends \Walker_Nav_Menu {
 }
 
 /**
+* ACF Options colour palette to JSON file
+*/
+add_action('acf/save_post', function ($post_id) {
+    if (current_user_can('administrator')) {
+        // Check if the updated post is the options page
+        if ($post_id == 'options') {
+            $colours = get_field('pf_colour_palette', 'option');
+            
+            $formattedArray = [];
+
+            foreach ($colours as $colour) {
+                $formattedArray[strtolower(str_replace(" ", "-", $colour['colour_name']))] = $colour['colour'];
+            }
+            // Convert the colours data to JSON format
+            $json_data = json_encode($formattedArray);
+            
+            // JSON colour file
+            $file_path = get_stylesheet_directory() . '/json/colours.json';
+
+            // Save the JSON data to the file
+            file_put_contents($file_path, $json_data);
+        }
+    }
+}, 20);
+
+/**
  * Add custom theme colours
  */
 add_action('acf/input/admin_footer', function() {
     ?>
     <script type="text/javascript">
     (function() {
-        acf.addFilter('color_picker_args', function(args, field) {
-            // Find a specific field
-            var fieldElement = document.querySelector('#color_primary');
+        acf.addFilter('color_picker_args', function(args, field) {    
+            // Get colours from colours.json
+            let colours = <?php echo file_get_contents(get_stylesheet_directory() . '/json/colours.json'); ?>;
+            let colourValues = Object.values(colours);
+                
+            args.palettes = colourValues;
 
-            // Do something to args
-            args.palettes = ['#5ee8bf', '#2f353e', '#f55e4f', '#f7f7f7', '#ffffff'];
-
-            // Return
             return args;
         });
     })();
