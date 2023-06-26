7e8c566505c8af6524b3967ba9897857e7af2572
Harry Gardiner Lunar <harry@lunar.build>
2023-06-26 08:45:43 +0100

    Updated footer theme option fields

--- web/app/themes/sage/app/options.php
+++ web/app/themes/sage/app/options.php
@@ -30,6 +30,42 @@ $theme_options
             'label' => 'Colour',
             'instructions' => 'Add a hex value for this colour. E.g. #ffffff',
             'required' => 1,
-        ]);
+        ])
+    ->endRepeater()
+    ->addTab('Social Media')
+    ->addLink('facebook_link', [
+        'label' => 'Facebook Link',
+        'instructions' => 'Add a link to the Facebook page.',
+    ])
+    ->addLink('twitter_link', [
+        'label' => 'Twitter Link',
+        'instructions' => 'Add a link to the Twitter page.',
+    ])
+    ->addLink('instagram_link', [
+        'label' => 'Instagram Link',
+        'instructions' => 'Add a link to the Instagram page.',
+    ])
+    ->addLink('youtube_link', [
+        'label' => 'YouTube Link',
+        'instructions' => 'Add a link to the YouTube page.',
+    ])
+    ->addLink('linkedin_link', [
+        'label' => 'LinkedIn Link',
+        'instructions' => 'Add a link to the LinkedIn page.',
+    ])
+
+    ->addTab('Footer')
+    ->addRepeater('pf_footer', [
+        'label' => 'Footer',
+        'instructions' => 'Add footer logos.',
+        'layout' => 'block',
+        'button_label' => 'Add Footer Logo',
+    ])
+        ->addImage('footer_logo', [
+            'label' => 'Footer Logo',
+            'instructions' => 'Add a logo for the footer.',
+        ])
+    ->endRepeater();
+
 
 acf_add_local_field_group($theme_options->build());
