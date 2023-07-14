a7ff0d8730634edba3d8ed15c985ba1870eb186b
Harry Gardiner Lunar <harry@lunar.build>
2023-07-14 15:32:26 +0100

    Removed h1 option from title partial. Added heading style option which applies the heading class. This class will be used to style the element.

--- web/app/themes/sage/app/Fields/Partials/Title.php
+++ web/app/themes/sage/app/Fields/Partials/Title.php
@@ -28,7 +28,19 @@ class Title extends Partial
                 'label' => 'Heading Level',
                 'required' => 1,
                 'choices' => [
-                    'h1' => 'H1',
+                    'h2' => 'H2',
+                    'h3' => 'H3',
+                    'h4' => 'H4',
+                    'h5' => 'H5',
+                    'h6' => 'H6',
+                ],
+                'default_value' => 'h2',
+            ])
+            ->addSelect('heading_style', [
+                'label' => 'Heading Style',
+                'instructions' => 'Select the heading style. This will change the font size and weight of the heading. The heading level does not have to match the heading style. E.g. you can have an H2 with a heading style of H4.',
+                'required' => 1,
+                'choices' => [
                     'h2' => 'H2',
                     'h3' => 'H3',
                     'h4' => 'H4',
--- web/app/themes/sage/resources/views/partials/title.blade.php
+++ web/app/themes/sage/resources/views/partials/title.blade.php
@@ -3,11 +3,12 @@
         $title = $title_style['title'];
         $heading_level = $title_style['heading_level'];
         $title_colour = $title_style['colour_picker'];
+        $heading_style = $title_style['heading_style'];
     }
 @endphp
 
 @if(isset($title_style))
     <div class="title">
-        <{{$title_style['heading_level']}} class="{{$title_colour}}">{{$title}}</{{$title_style['heading_level']}}>
+        <{{$title_style['heading_level']}} class="{{$title_colour}} {{$heading_style}}">{{$title}}</{{$title_style['heading_level']}}>
     </div>
 @endif
