467ea9b54c90c6b67f761709ed20498c364d61b3
Ellie Hobbs <eshobbs7@gmail.com>
2023-07-14 14:55:18 +0100

    Put fallback around title block

--- web/app/themes/sage/resources/views/partials/title.blade.php
+++ web/app/themes/sage/resources/views/partials/title.blade.php
@@ -1,8 +1,13 @@
 @php
-    $title = $title_style['title'];
-    $heading_level = $title_style['heading_level'];
-    $title_colour = $title_style['colour_picker'];
+    if(isset($title_style)) {
+        $title = $title_style['title'];
+        $heading_level = $title_style['heading_level'];
+        $title_colour = $title_style['colour_picker'];
+    }
 @endphp
-<div class="title">
-    <{{$title_style['heading_level']}} class="{{$title_colour}}">{{$title}}</{{$title_style['heading_level']}}>
-</div>
+
+@if(isset($title_style))
+    <div class="title">
+        <{{$title_style['heading_level']}} class="{{$title_colour}}">{{$title}}</{{$title_style['heading_level']}}>
+    </div>
+@endif
