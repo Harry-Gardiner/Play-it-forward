c50e3587352fe39f86a0fade46dcea49ab37b199
Harry Gardiner Lunar <harry@lunar.build>
2023-07-21 16:14:59 +0100

    Reworked card width and height to resolve responsive issues

--- web/app/themes/sage/resources/views/partials/card.blade.php
+++ web/app/themes/sage/resources/views/partials/card.blade.php
@@ -9,10 +9,10 @@
 <div class="card">
     <a href="{{get_permalink($post->ID)}}">
         <div class="card__image">
-            <img src="{{get_the_post_thumbnail_url($post->ID, 'medium')}}" alt="">  
+            <img src="{{get_the_post_thumbnail_url($post->ID)}}" alt="">  
         </div>
         <div class="card__content">
-            <h3 class="card__title"><span>{{$post->post_title}}</span></h3>
+            <h3 class="card__title h2"><span>{{$post->post_title}}</span></h3>
             <p class="card__excerpt">{{$post->post_excerpt}}</p>    
         </div>
     </a>
