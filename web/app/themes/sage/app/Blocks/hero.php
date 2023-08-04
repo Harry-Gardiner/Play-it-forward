c574ead9784bbd221e872ddf5c0f9ade1c3beb91
Harry Gardiner Lunar <harry@lunar.build>
2023-08-04 15:43:00 +0100

    Basic hero fields added

---
+++ web/app/themes/sage/app/Blocks/hero.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Blocks;
+
+use Log1x\AcfComposer\Block;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+class hero extends Block
+{
+    /**
+     * The block name.
+     *
+     * @var string
+     */
+    public $name = 'Hero';
+
+    /**
+     * The block description.
+     *
+     * @var string
+     */
+    public $description = 'Hero block, displayed at the top of pages.';
+
+    /**
+     * The block category.
+     *
+     * @var string
+     */
+    public $category = 'formatting';
+
+    /**
+     * The block icon.
+     *
+     * @var string|array
+     */
+    public $icon = 'cover-image';
+
+    /**
+     * The block keywords.
+     *
+     * @var array
+     */
+    public $keywords = ['hero', 'header', 'title', 'image'];
+
+    /**
+     * The block post type allow list.
+     *
+     * @var array
+     */
+    public $post_types = [];
+
+    /**
+     * The parent block type allow list.
+     *
+     * @var array
+     */
+    public $parent = [];
+
+    /**
+     * The default block mode.
+     *
+     * @var string
+     */
+    public $mode = 'auto';
+
+    /**
+     * The default block alignment.
+     *
+     * @var string
+     */
+    public $align = '';
+
+    /**
+     * The default block text alignment.
+     *
+     * @var string
+     */
+    public $align_text = '';
+
+    /**
+     * The default block content alignment.
+     *
+     * @var string
+     */
+    public $align_content = '';
+
+    /**
+     * The supported block features.
+     *
+     * @var array
+     */
+    public $supports = [
+        'align' => true,
+        'align_text' => false,
+        'align_content' => false,
+        'full_height' => false,
+        'anchor' => false,
+        'mode' => false,
+        'multiple' => true,
+        'jsx' => true,
+    ];
+
+    /**
+     * The block styles.
+     *
+     * @var array
+     */
+    public $styles = [
+        [
+            'name' => 'light',
+            'label' => 'Light',
+            'isDefault' => true,
+        ],
+        [
+            'name' => 'dark',
+            'label' => 'Dark',
+        ]
+    ];
+
+    /**
+     * The block preview example data.
+     *
+     * @var array
+     */
+    public $example = [
+        'items' => [
+            ['item' => 'Item one'],
+            ['item' => 'Item two'],
+            ['item' => 'Item three'],
+        ],
+    ];
+
+    /**
+     * Data to be passed to the block before rendering.
+     *
+     * @return array
+     */
+    public function with()
+    {
+        return [
+           
+        ];
+    }
+
+    /**
+     * The block field group.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        $hero = new FieldsBuilder('hero');
+
+        $hero
+        ->addMessage('block_title', '', [
+            'label' => 'Hero',
+        ])
+        ->addFields($this->get(GeneralTab::class))  
+        ->addTab('Hero Content')
+        ->addImage('hero_image', [
+            'label' => 'Hero Image',
+            'return_format' => 'array',
+            'preview_size' => 'medium',
+            'library' => 'all',
+            'mime_types' => 'jpg, jpeg, png, svg',
+        ]) 
+        ->addWysiwyg('hero_content', [
+            'label' => 'Hero Text',
+            'media_upload' => 0,
+            'tabs' => 'visual',
+            'toolbar' => 'basic',
+            'delay' => 1,
+            'wrapper' => [
+                'class' => 'autosize',
+            ],
+        ])
+        ;
+
+        return $hero->build();
+    }
+
+    /**
+     * Assets to be enqueued when rendering the block.
+     *
+     * @return void
+     */
+    public function enqueue()
+    {
+        //
+    }
+}
