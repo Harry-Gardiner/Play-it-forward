74b46e309d210a04605ecb8ce6654158652e42d6
Harry Gardiner Lunar <harry@lunar.build>
2023-08-18 16:47:54 +0100

    Logo text no longer visisble on mobile. Text is visible on lg.

--- web/app/themes/sage/resources/views/partials/logo.blade.php
+++ web/app/themes/sage/resources/views/partials/logo.blade.php
@@ -1,6 +1,6 @@
 <a class="logo" href="{{ home_url('/') }}">
     <div class="logo__wrapper">
         <img src="{{ asset('images/pif_logo_black.svg') }}" alt="Play it forward logo">
-        {{-- <p>play it <span class="strong">forward</span></p> --}}
+        <p class="logo__wrapper__text">play it <span class="strong">forward</span></p>
     </div>
 </a>
