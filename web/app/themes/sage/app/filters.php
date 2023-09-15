3f6a796581ca118328a7b6a6cadb4475401956fd
Harry Gardiner Lunar <harry@lunar.build>
2023-09-15 16:47:17 +0100

    Merged restricted blocks

--- web/app/themes/sage/app/filters.php
+++ web/app/themes/sage/app/filters.php
@@ -15,13 +15,45 @@ add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
 
-add_filter('acf/load_field/name=latest_posts_type', function($field) {
+add_filter('acf/load_field/name=latest_posts_type', function ($field) {
     $post_types = get_post_types(array('public' => true), 'names');
     unset($post_types['attachment']);
-    foreach ( $post_types as $post_type ) {
+    foreach ($post_types as $post_type) {
         $field['choices'][$post_type] = $post_type;
-     }
- 
-     // return the field
-     return $field;
+    }
+
+    // return the field
+    return $field;
 });
+
+add_filter('allowed_block_types_all', function ($block_editor_context, $editor_context) {
+    // Core Gutenberg blocks.
+    $core_blocks = array(
+        'core/paragraph',
+        'core/heading',
+        'core/table',
+        'core/image',
+        'core/video',
+        'core/search',
+        'core/spacer',
+    );
+
+
+    // Fetch all ACF blocks.
+    $block_files = glob(__DIR__ . '/Blocks/*.php');
+    $acf_blocks = array();
+    foreach ($block_files as $block_file) {
+        $block_name = basename($block_file, '.php');
+        $block_name = strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $block_name));
+        $acf_blocks[] = 'acf/' . $block_name;
+    }
+
+    // Merge allowed blocks.
+    $allowed_blocks = array_merge($core_blocks, $acf_blocks);
+
+    if (!empty($editor_context->post)) {
+        return $allowed_blocks;
+    }
+
+    return $block_editor_context;
+}, 10, 2);
