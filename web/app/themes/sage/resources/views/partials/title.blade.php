7ba256e3833a4f11d847d19dc0c7584e775ca937
Harry Gardiner Lunar <harry@lunar.build>
2023-09-22 14:33:14 +0100

    Removed title colour option.

--- web/app/themes/sage/resources/views/partials/title.blade.php
+++ web/app/themes/sage/resources/views/partials/title.blade.php
@@ -1,14 +1,15 @@
 @php
-    if(isset($title_style)) {
+    if (isset($title_style)) {
         $title = $title_style['title'];
         $heading_level = $title_style['heading_level'];
-        $title_colour = $title_style['colour_picker'];
         $heading_style = $title_style['heading_style'];
     }
 @endphp
 
-@if(isset($title_style))
+@if (isset($title_style))
     <div class="title">
-        <{{$title_style['heading_level']}} class="{{$title_colour}} {{$heading_style}}">{{$title}}</{{$title_style['heading_level']}}>
+        <{{ $title_style['heading_level'] }} {{ $heading_style }}>
+            {{ $title }}
+            </{{ $title_style['heading_level'] }}>
     </div>
 @endif
