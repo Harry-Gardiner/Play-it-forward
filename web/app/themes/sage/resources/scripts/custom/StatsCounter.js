0363c78c557ed0bc0ddff6b524f4a39358c8ac9e
Harry Gardiner Lunar <harry@lunar.build>
2023-10-13 17:02:06 +0100

    Added some additional count up options

--- web/app/themes/sage/resources/scripts/custom/StatsCounter.js
+++ web/app/themes/sage/resources/scripts/custom/StatsCounter.js
@@ -3,18 +3,21 @@ import { CountUp } from 'countup.js';
 function animateNumbers() {
     const options = {
         startVal: 0,
-        duration: 2,
+        duration: 3,
         separator: ',',
         enableScrollSpy: true,
-        scrollSpyDelay: 200,
         scrollSpyOnce: true,
+        useEasing: true,
+        smartEasingThreshold: 999,
+        smartEasingAmount: 333,
     };
 
     const numbers = document.querySelectorAll('.custom-grid__stat__number');
     numbers.forEach((number) => {
-        console.log(number.textContent);
-
         const countUp = new CountUp(number.id, Number(number.textContent), options);
+
+        // after DOM has rendered
+        countUp.handleScroll();
 
         if (!countUp.error) {
             countUp.start();
