9940e63d38d6bb39f68772121626313076d1b830
Harry Gardiner Lunar <harry@lunar.build>
2023-10-27 14:15:49 +0100

    Added some spacing to the donation form

--- web/app/themes/sage/resources/views/blocks/donate.blade.php
+++ web/app/themes/sage/resources/views/blocks/donate.blade.php
@@ -1,11 +1,16 @@
-@php
+<script>
+    ! function(e, t) {
+        if (!e.getElementById(t)) {
+            var c = e.createElement("script");
+            c.id = t, c.src = "https://static.beaconproducts.co.uk/js-sdk/production/beaconcrm.min.js", e
+                .getElementsByTagName("head")[0].appendChild(c)
+        }
+    }(document, "beacon-js-sdk")
+</script>
 
-@endphp
-
-<section
-    class="custom-grid {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--white full-bleed">
+<section class="donate {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--off-white full-bleed">
     <div class="block-padding container">
-        <div class="custom-grid__content">
+        <div class="donate__content">
             @if ($title_style['title'])
                 @include('partials.title', [$title_style])
             @endif
