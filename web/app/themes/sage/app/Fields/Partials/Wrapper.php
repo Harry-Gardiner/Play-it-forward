2dba7936ce8758184185125d1141257e9f9bbed8
Harry Gardiner Lunar <harry@lunar.build>
2023-11-06 07:28:14 +0000

    Update blog hero block styles and wrapper default spacing

--- web/app/themes/sage/app/Fields/Partials/Wrapper.php
+++ web/app/themes/sage/app/Fields/Partials/Wrapper.php
@@ -26,7 +26,7 @@ class Wrapper extends Partial
                     'block-spacing--auto' => 'Auto',
                     'block-spacing--none' => 'None',
                 ],
-                'default' => 'block-spacing--both',
+                'default' => 'block-spacing--none',
                 'wpml_cf_preferences' => 0,
             ])
             ->addSelect('spacing_size', [
--- web/app/themes/sage/resources/views/blocks/blog-hero.blade.php
+++ web/app/themes/sage/resources/views/blocks/blog-hero.blade.php
@@ -14,13 +14,15 @@
         $image_position = $hero_image_position ?? 'center center';
     }
 
+    // Meta
+    $categories = get_the_category();
+
     // Content
     $hero_title = get_the_title() ?? null;
 @endphp
 
 <section
     class="blog-hero full-bleed {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--{{ $background_colour }}">
-
     @if ($hero_image)
         <div class="blog-hero__image">
             <img src="{{ $hero_image }}" alt="{{ $alt_text }}" style="object-position:{{ $hero_image_position }}">
@@ -29,7 +31,16 @@
 
     @if ($hero_title)
         <div class="blog-hero__inner container">
-            <div class="blog-hero__inner__content flow">
+            <div class="flow">
+                <div class="blog-hero__inner__content">Blog <span>|</span>
+                    @if (!empty($categories))
+                        <div class="blog-hero__inner__categories">
+                            @foreach ($categories as $category)
+                                <span>{{ $category->name }}</span>
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
                 <div class="blog-hero__title">
                     <h1 class="giant-h1">{!! $hero_title !!}</h1>
                 </div>
