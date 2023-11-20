02e06520484c2bf83de9f2103eebaa7ca4ce4d98
Harry Gardiner Lunar <harry@lunar.build>
2023-11-20 09:06:25 +0000

    Basic 404 added.

--- web/app/themes/sage/resources/views/404.blade.php
+++ web/app/themes/sage/resources/views/404.blade.php
@@ -5,12 +5,19 @@
 
   @if (! have_posts())
     
-      <div class="error-404 full-bleed">
+      <div class="error-404 full-bleed block-padding">
         <div class="error-404__circle">
-          <p>404</p>
-          <p>Sorry, page not found</p>
+          <p class="h1">404</p>
+          <p class="h2">Sorry, page not found</p>
         </div>
       </div>
-
+      <div class="home-button block-padding--bottom">
+        @include('partials.button', [
+          'type' => 'button',
+          'link' => '/',
+          'text' => 'Back to home',
+          'colour' => 'bg-raspberry',
+            ])
+      </div>
   @endif
 @endsection
