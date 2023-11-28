43e85373191ef7e618b3f34a562b26a8c8b27415
Harry Gardiner Lunar <harry@lunar.build>
2023-11-28 22:08:16 +0000

    Added embla auto play

--- web/app/themes/sage/resources/scripts/custom/Slider.js
+++ web/app/themes/sage/resources/scripts/custom/Slider.js
@@ -1,5 +1,6 @@
 import EmblaCarousel from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay'
 
 const emblaNode = document.querySelector('.embla')
 const options = { loop: true }
-const emblaApi = EmblaCarousel(emblaNode, options)
+const emblaApi = EmblaCarousel(emblaNode, options, [Autoplay()])
