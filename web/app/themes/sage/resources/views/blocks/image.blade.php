b0b380fbdf43bc23aef1ffe0c234b6483b0770df
Harry Gardiner Lunar <harry@lunar.build>
2023-11-29 18:05:25 +0000

    Removed old image block

--- web/app/themes/sage/resources/views/blocks/image.blade.php
+++
@@ -1,4 +0,0 @@
-<section
-    class="full-image {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--{{ $background_colour }} full-bleed">
-    @include('partials.image', ['image' => $image])
-</section>
