523538f7c9495e198d90d2c54783cb5150d04a30
Ellie Hobbs <eshobbs7@gmail.com>
2023-12-01 13:08:46 +0000

    Text spacing tweaks

--- web/app/themes/sage/resources/views/blocks/text.blade.php
+++ web/app/themes/sage/resources/views/blocks/text.blade.php
@@ -1,5 +1,5 @@
 <div class="{{ $block->classes }} {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
     @if(!empty($text))
-        <div class="text__{{ $style }} block-padding">{!! $text !!}</div>
+        <div class="text__{{ $style }}">{!! $text !!}</div>
     @endif
 </div>
