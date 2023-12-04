c4959ab088bc4a7a00180f93a4846fc7d7c1be8b
Harry Gardiner Lunar <harry@lunar.build>
2023-12-04 15:37:49 +0000

    Hero desktop layout added

--- web/app/themes/sage/resources/views/blocks/hero.blade.php
+++ web/app/themes/sage/resources/views/blocks/hero.blade.php
@@ -14,44 +14,19 @@
 
     // Content
     $hero_title = $hero_title ?? null;
-    $highlighted_text = $highlighted_text ?? null;
     $hero_content = $hero_content ?? null;
-
-    if ($highlighted_text && !empty($highlighted_text)) {
-        foreach ($highlighted_text as $word) {
-            $word = $word['text_string'];
-            $hero_title = str_replace($word, '<span class="highlight">' . $word . '</span>', $hero_title);
-        }
-    }
-
-    // Btn
-    if ($show_button == 'yes') {
-        $btn_link = $cta_button['link'] ?? null;
-        $btn_text = $cta_button['text'] ?? null;
-        $btn_colour = 'raspberry';
-        $btn_type = $cta_button['type'] ?? null;
-    }
 
 @endphp
 <section class="hero full-bleed {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
 
     @if ($hero_title)
-        <div class="hero__inner">
-            <div class="hero__inner__content flow">
+        <div class="hero__inner container flow">
                 <div class="hero__title">
-                    <h1 class="giant-h1">{!! $hero_title !!}</h1>
+                    <h1 class="h1">{!! $hero_title !!}</h1>
                 </div>
-                @if ($show_button == 'yes')
-                    <div class="hero__sub-text flow">
-                        @include('partials.button', [
-                            'type' => $btn_type,
-                            'link' => $btn_link,
-                            'text' => $btn_text,
-                            'colour' => $btn_colour,
-                        ])
-                    </div>
-                @endif
-            </div>
+                <div class="hero__content">
+                    {!! $hero_content !!}
+                </div>
         </div>
     @endif
 
