99fae1825345ca822ef31a09f23620c5286a5fe1
Harry Gardiner Lunar <harry@lunar.build>
2023-12-15 15:36:53 +0000

    Added catch to Results block when no match results for a team.

--- web/app/themes/sage/resources/views/blocks/results.blade.php
+++ web/app/themes/sage/resources/views/blocks/results.blade.php
@@ -1,42 +1,45 @@
 @php
-    $team_one_matches = !empty(get_field('matches', $team_one->ID)) ? get_field('matches', $team_one->ID) : null;
-    $team_two_matches = !empty(get_field('matches', $team_two->ID)) ? get_field('matches', $team_two->ID) : null;
+$team_one_matches = !empty(get_field('matches', $team_one->ID)) ? get_field('matches', $team_one->ID) : null;
+$team_two_matches = !empty(get_field('matches', $team_two->ID)) ? get_field('matches', $team_two->ID) : null;
 
-    // Match function
-    if (!function_exists('getLastValidMatch')) {
-    function getLastValidMatch($matches)
-    {
-        usort($matches, function ($a, $b) {
-            $dateA = DateTime::createFromFormat('d/m/Y', $a['match_date']);
-            $dateB = DateTime::createFromFormat('d/m/Y', $b['match_date']);
+// dd($team_one_matches, $team_two_matches);
 
-            return $dateA <=> $dateB;
-        });
+// Match function
+if (!function_exists('getLastValidMatch')) {
+function getLastValidMatch($matches)
+{
+usort($matches, function ($a, $b) {
+$dateA = DateTime::createFromFormat('d/m/Y', $a['match_date']);
+$dateB = DateTime::createFromFormat('d/m/Y', $b['match_date']);
 
-        do {
-            $last_match = array_pop($matches);
-        } while ($last_match && ($last_match['match_score'] === '' || $last_match['match_opponent'] === ''));
+return $dateA <=> $dateB;
+    });
 
-        if ($last_match) {
-            // split match score into array by the - character
-            $score = explode('-', $last_match['match_score']);
-            $last_match['home_score'] = $score[0];
-            $last_match['away_score'] = $score[1];
-        }
+    do {
+    $last_match = array_pop($matches);
+    } while ($last_match && ($last_match['match_score'] === '' || $last_match['match_opponent'] === ''));
 
-        return $last_match;
+    if ($last_match) {
+    // split match score into array by the - character
+    $score = explode('-', $last_match['match_score']);
+    $last_match['home_score'] = $score[0];
+    $last_match['away_score'] = $score[1];
     }
-  }
+
+    return $last_match;
+    }
+    }
 
     // Lates match results
-    $team_one_last_match = getLastValidMatch($team_one_matches);
-    $team_two_last_match = getLastValidMatch($team_two_matches);
+    $team_one_last_match = $team_one_matches ? getLastValidMatch($team_one_matches) : null;
+    $team_two_last_match = $team_two_matches ? getLastValidMatch($team_two_matches) : null;
 
     $teams = [$team_one_last_match, $team_two_last_match];
-@endphp
+    @endphp
 
-@if ($team_one_matches && $team_two_matches)
-    <section class="results {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--raspberry full-bleed">
+    {{-- @if ($team_one_matches && $team_two_matches) --}}
+    <section
+        class="results {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--raspberry full-bleed">
         <div class="results__wrapper container">
             <div class="results__tabs full-bleed">
                 <button id="defaultOpen" class="results__tab active" data-team="team-one">
@@ -52,14 +55,15 @@
                     <p class="h2 results__content__title">{{ $team_one_title }}</p>
                     <div class="results__body">{!! $team_one_body !!}</div>
                     @include('partials.button', [
-                      'type' => 'button',
-                      'link' => get_permalink($team_one->ID),
-                      'text' => 'Find out more',
-                      'colour' => 'white',
-                        ])
+                    'type' => 'button',
+                    'link' => get_permalink($team_one->ID),
+                    'text' => 'Find out more',
+                    'colour' => 'white',
+                    ])
                 </div>
+                @if ($team_one_matches && $team_two_matches)
                 <div class="results__recent">
-                    <div class="football-team__latest__content block-padding">
+                    <div class="football-team__latest__content block-padding--top">
                         <div class="title">
                             <h2 class="h2 football-team__results__heading">
                                 Recent match results
@@ -81,45 +85,48 @@
                         </div>
                     </div>
                 </div>
+                @endif
             </div>
 
             <div id="team-two" class="results__team block-padding--top">
-              <div class="results__content flow">
-                  <p class="h2 results__content__title">{{ $team_two_title }}</p>
-                  <div class="results__body">{!! $team_two_body !!}</div>
-                  @include('partials.button', [
+                <div class="results__content flow">
+                    <p class="h2 results__content__title">{{ $team_two_title }}</p>
+                    <div class="results__body">{!! $team_two_body !!}</div>
+                    @include('partials.button', [
                     'type' => 'button',
                     'link' => get_permalink($team_two->ID),
                     'text' => 'Find out more',
                     'colour' => 'white',
-                      ])
-              </div>
-              <div class="results__recent">
-                  <div class="football-team__latest__content block-padding">
-                      <div class="title">
-                          <h2 class="h2 football-team__results__heading">
-                              Recent match results
-                          </h2>
-                      </div>
-                      <div class="football-team__latest__last">
-                          <p class="h1 score-top">
-                              {{ $team_two_last_match['home_score'] }}-{{ $team_two_last_match['away_score'] }}
-                          </p>
-                          <div class="football-team__latest__score">
-                              <p class="team h1">{{ $team_two_last_match['home_score'] }}</p>
-                              <p class="h3">Play it forward fc</p>
-                          </div>
-                          <p class="vs">VS</p>
-                          <div class="football-team__latest__score">
-                              <p class="team h1">{{ $team_two_last_match['away_score'] }}</p>
-                              <p class="h3">{{ $team_two_last_match['match_opponent'] }}</p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          </div>
+                    ])
+                </div>
+                @if ($team_one_matches && $team_two_matches)
+                <div class="results__recent">
+                    <div class="football-team__latest__content block-padding--top">
+                        <div class="title">
+                            <h2 class="h2 football-team__results__heading">
+                                Recent match results
+                            </h2>
+                        </div>
+                        <div class="football-team__latest__last">
+                            <p class="h1 score-top">
+                                {{ $team_two_last_match['home_score'] }}-{{ $team_two_last_match['away_score'] }}
+                            </p>
+                            <div class="football-team__latest__score">
+                                <p class="team h1">{{ $team_two_last_match['home_score'] }}</p>
+                                <p class="h3">Play it forward fc</p>
+                            </div>
+                            <p class="vs">VS</p>
+                            <div class="football-team__latest__score">
+                                <p class="team h1">{{ $team_two_last_match['away_score'] }}</p>
+                                <p class="h3">{{ $team_two_last_match['match_opponent'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endif
+            </div>
+        </div>
 
         </div>
     </section>
-@endif
+    {{-- @endif --}}
