bf3d3a3da2ef81df39b61e54bed1b77edb240848
Harry Gardiner Lunar <harry@lunar.build>
2024-01-03 23:00:44 +0000

    Reworked page hero to use blog hero code and styling.

--- web/app/themes/sage/resources/views/blocks/hero.blade.php
+++ web/app/themes/sage/resources/views/blocks/hero.blade.php
@@ -6,10 +6,11 @@
 
     // Image
     $hero_image = $hero_image ?? null;
+    // dump($hero_image);
     $alt_text = '';
-    if (is_array($hero_image) && isset($hero_image['alt'])) {
-        $alt_text = $hero_image['alt'] !== '' ? $hero_image['alt'] : 'hero image';
-    }
+    // if (is_array($hero_image) && isset($hero_image['alt'])) {
+    //     $alt_text = $hero_image['alt'] !== '' ? $hero_image['alt'] : 'hero image';
+    // }
     $image_position = $hero_image_position ?? 'center center';
 
     // Content
@@ -17,7 +18,7 @@
     $hero_content = $hero_content ?? null;
 
 @endphp
-<section class="hero {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
+{{-- <section class="hero {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
 
     @if ($hero_title)
         <div class="hero__inner flow">
@@ -37,4 +38,33 @@
         </div>
     @endif
 
-</section>
+</section> --}}
+
+
+<section
+    class="blog-hero full-bleed {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--{{ $background_colour }} {{ $show_hero_image !== 'yes' ? 'base' : 'has-image' }}">
+    
+    @if ($hero_title)
+        <div class="blog-hero__inner container">
+            <div class="blog-hero__wrapper flow">
+                <div class="blog-hero__title">
+                    <h1 class="h1">{!! $hero_title !!}</h1>
+                </div>
+                @if ($hero_content)
+                    <div>{!! $hero_content !!}</div>
+                @endif
+            </div>
+        </div>
+    @endif
+
+    @if ($show_hero_image == 'yes' && $hero_image)
+        <div class="blog-hero__image">
+            <img src="{{ $hero_image['url'] }}" alt="{{ $alt_text }}" style="object-position:{{ $image_position }}">
+        </div>
+    @else
+       <div class="blog-hero__image--base">
+        <img src="{{ asset('images/pif_logo_lrg_white.png') }}"
+        alt="Play it forward logo">
+       </div>
+    @endif
+    </section>
