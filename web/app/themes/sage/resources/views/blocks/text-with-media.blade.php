6a96eb1137af64ebb27e38c0e821624a49957fd6
Ellie Hobbs <eshobbs7@gmail.com>
2024-01-05 16:28:14 +0000

    Started making text with media block

---
+++ web/app/themes/sage/resources/views/blocks/text-with-media.blade.php
@@ -0,0 +1,33 @@
+@php
+    // Impact word
+    if ($impact_word_enable === 'yes') {
+        $impact = $impact_word_position;
+    }
+@endphp
+
+<section class="text-with-media {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }} bg--{{ $background_colour }} full-bleed">
+    <div class="{{ $impact_word_enable === 'yes' ? 'impact impact--' . $impact : '' }} block-padding container">
+        @if ($impact_word_enable === 'yes')
+            <div class="impact__word">{{ $impact_word }}</div>
+        @endif
+        <div class="text-with-media__content">
+            @if ($title_style['title'])
+                <div class="text-with-media__content__title title-{{ $title_align }}">
+                    @include('partials.title', [$title_style])
+                </div>
+            @endif
+
+            @if($body)
+                <div class="text-with-media__content__body">
+                    {!! $body !!}
+                </div>
+            @endif
+
+            @if($media_type === 'image') 
+                <img class="text-with-media__content__image" src={{ $image['url'] }} alt={{ $image['alt'] }}/>
+            @elseif($media_type === 'video')
+                {{-- <video/> --}}
+            @endif
+
+        </div>
+</section>
