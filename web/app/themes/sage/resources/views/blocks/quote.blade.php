4d923710ee43de3fa30118b9bd1ee912342ee150
Harry Gardiner Lunar <harry@lunar.build>
2024-01-08 21:59:12 +0000

    Styled two column quote to match block quote.

--- web/app/themes/sage/resources/views/blocks/quote.blade.php
+++ web/app/themes/sage/resources/views/blocks/quote.blade.php
@@ -1,7 +1,7 @@
 <section class="{{ $block->classes }} {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
-    @if(isset($text))
-        <p class="quote__{{ $style }}">{!! $text !!}</p>
-        @if(isset($author))
+    @if (isset($text))
+        <p class="quote quote__{{ $style }}"><span class="quote__mark"><span class="quote__mark--before">“</span></span>{!! $text !!}<span class="quote__mark"><span class="quote__mark--after">”</span></span></p>
+        @if (isset($author))
             <p class="quote__author">{!! $author !!}</p>
         @endif
     @endif
