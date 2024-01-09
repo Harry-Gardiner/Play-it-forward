07261cf09653bf4128e1290b20ac153bc1bcae7b
Harry Gardiner Lunar <harry@lunar.build>
2024-01-09 21:43:29 +0000

    Added flow to text WYSIWYG content

--- web/app/themes/sage/resources/views/blocks/text.blade.php
+++ web/app/themes/sage/resources/views/blocks/text.blade.php
@@ -1,5 +1,5 @@
 <div class="{{ $block->classes }} {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
     @if(!empty($text))
-        <div class="text__{{ $style }}">{!! $text !!}</div>
+        <div class="text__{{ $style }} flow">{!! $text !!}</div>
     @endif
 </div>
