68f5a9ca71177a93e87cf585d285f9a47ac9cc22
Harry Gardiner Lunar <harry@lunar.build>
2024-03-12 08:53:19 +0000

    Removed glider-js, embla already installed and working. Adapting block to use this.

--- site/web/app/themes/sage/app/Blocks/Carousel.php
+++ site/web/app/themes/sage/app/Blocks/Carousel.php
@@ -145,14 +145,8 @@ class Carousel extends Block
 
             // Carousel
             'slides' => get_field('slides'),
-            'slider_type' => get_field('slider_type'),
-            'full' => get_field('full'),
-            'slider_autoplay' => get_field('slider_autoplay'),
-            'add_text' => get_field('add_text'),
-            'slider_ratio' => get_field('slider_ratio'),
-            'slide_gap' => get_field('slide_gap'),
-            'slide_reveal' => get_field('slide_reveal'),
-            'num_of_shown_slides' => get_field('num_of_shown_slides'),
+            'slider_type' => get_field('slider_type'), 
+                      
         ];
     }
 
@@ -182,103 +176,37 @@ class Carousel extends Block
                 'multiple' => 0,
                 'return_format' => 'value',
             ])
-            ->addRadio('full', [
-                'label' => 'Full Width',
-                'instructions' => 'Full width will ignore the container width and stretch the slider to the full width of the screen.',
-                'choices' => [
-                    'true' => 'Yes',
-                    'false' => 'No',
-                ],
-                'default_value' => 'false',
-                'layout' => 'horizontal',
-            ])
-            ->addRadio('slider_autoplay', [
-                'label' => 'Autoplay',
-                'choices' => [
-                    'true' => 'Yes',
-                    'false' => 'No',
-                ],
-                'default_value' => 'false',
-                'layout' => 'horizontal',
-            ])->conditional('slider_type', '==', 'slider-type-images')
-            ->addSelect('slider_ratio', [
-                'label' => 'Slider Ratio',
-                'instructions' => 'Select the ratio of the slider. 21:9 is the default.',
-                'choices' => [
-                    'slider-item-ratio-21x9' => '21x9',
-                    'slider-item-ratio-2x1' => '2x1',
-                    'slider-item-ratio-16x9' => '16x9',
-                    'slider-item-ratio-4x3' => '4x3',
-                    'slider-item-ratio-1x1' => '1x1',
-                    'slider-item-ratio-3x4' => '3x4',
-                    'slider-item-ratio-32x9' => '32x9',
-                ],
-                'default_value' => 'slider-item-ratio-21x9',
-                'multiple' => 0,
-                'return_format' => 'value',
-            ])->conditional('slider_type', '==', 'slider-type-images')
-            ->addRadio('slide_gap', [
-                'label' => 'Slide Gap',
-                'instructions' => 'Add a gap between the slides.',
-                'choices' => [
-                    'true' => 'Yes',
-                    'false' => 'No',
-                ],
-                'default_value' => 'false',
-                'layout' => 'horizontal',
-            ])->conditional('slider_type', '==', 'slider-type-images')
-            ->addRadio('slide_reveal', [
-                'label' => 'Slide Reveal',
-                'instructions' => 'Add a reveal effect to the slides. This will reveal a portion on the next slide.',
-                'choices' => [
-                    'true' => 'Yes',
-                    'false' => 'No',
-                ],
-                'default_value' => 'false',
-                'layout' => 'horizontal',
-            ])->conditional('slider_type', '==', 'slider-type-images')
-            ->addSelect('num_of_shown_slides', [
-                'label' => 'Number of Shown Slides',
-                'instructions' => 'Select the number of slides to show at once.',
-                'choices' => [
-                    '1' => '1',
-                    '2' => '2',
-                    '3' => '3',
-                    '4' => '4',
-                    '5' => '5'
-                ],
-                'default_value' => '1',
-                'multiple' => 0,
-                'return_format' => 'value',
-            ])
             ->addTab('Slides')
             ->addRepeater('slides', [
                 'label' => 'Slides',
-                'layout' => 'row',
+                'layout' => 'block',
                 'button_label' => 'Add Slide',
             ])
-                ->addImage('image', [
-                    'label' => 'Image',
-                    'return_format' => 'array',
-                    'preview_size' => 'medium',
-                    'library' => 'all',
-                ])
-                ->addRadio('add_text', [
-                    'label' => 'Add Text',
-                    'instructions' => 'Add a title and subtitle to the slide. Will be positioned centrally.',
-                    'choices' => [
-                        'true' => 'Yes',
-                        'false' => 'No',
-                    ],
-                    'default_value' => 'false',
-                    'layout' => 'horizontal',
-                ])
-                ->addText('title', [
-                    'label' => 'Title',
-                ])->conditional('add_text', '==', 'true')
-                ->addText('subtitle', [
-                    'label' => 'Subtitle',
-                ])->conditional('add_text', '==', 'true')
+            ->addImage('image', [
+                'label' => 'Image',
+                'instructions' => 'Upload an image for the slide.',
+                'return_format' => 'array',
+                'preview_size' => 'thumbnail',
+                'library' => 'all',
+                'mime_types' => 'jpg, jpeg, png, gif',
+            ])
+            ->addText('title', [
+                'label' => 'Title',
+                'instructions' => 'Add a title for the slide.',
+            ])
+            ->addText('subtitle', [
+                'label' => 'Subtitle',
+                'instructions' => 'Add a subtitle for the slide.',
+            ])
+            ->addText('link', [
+                'label' => 'Link',
+                'instructions' => 'Add a link for the slide.',
+            ])
+            ->addText('link_text', [
+                'label' => 'Link Text',
+                'instructions' => 'Add a link text for the slide.',
+            ])
+            ->endRepeater()
         ;
         return $carousel->build();
     }
--- site/web/app/themes/sage/resources/scripts/custom/Slider.js
+++ site/web/app/themes/sage/resources/scripts/custom/Slider.js
@@ -1,9 +1,16 @@
 import EmblaCarousel from 'embla-carousel'
 import Autoplay from 'embla-carousel-autoplay'
 
+// Front page hero slider
 const emblaNode = document.querySelector('.embla')
 const options = { loop: true }
 if (emblaNode) {
     const emblaApi = EmblaCarousel(emblaNode, options, [Autoplay()])
 }
 
+// Carousel slider
+const carouselNode = document.querySelector('.carousel')
+const carouselOptions = { loop: true, slidesToScroll: 1, slidesToShow: 3 }
+if (carouselNode) {
+    const carousel = EmblaCarousel(carouselNode, carouselOptions, [Autoplay()])
+}
