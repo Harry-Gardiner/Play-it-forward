3e7c291f01eb8973adde64f751310a5c97ba23fd
Harry Gardiner Lunar <harry@lunar.build>
2024-03-13 08:46:17 +0000

    Added base html for card row

---
+++ site/web/app/themes/sage/resources/views/blocks/card-row.blade.php
@@ -0,0 +1,35 @@
+@php
+dump($items);
+@endphp
+<section class="card-row {{ $wrapper ? $wrapper : '' }} {{ $spacing_size ? $spacing_size : '' }}">
+   <div class="container">
+      @if ($title_style['title'] !== '')
+          @include('partials.title', [$title_style])
+      @endif
+   </div>
+   <div class="card-row__grid">
+      @foreach ($items as $item)
+          <div class="card-row__item">
+              <div class="card-row__item__inner">
+                  <div class="card-row__item__content">
+                      <img src="{{ $item['image']['sizes']['medium_large'] }}""
+                      alt="{{ $item['image']['alt'] ? $item['image']['alt'] : $item['image']['name'] }}">
+                      <div class="card-row__item__body">
+                        @if ($item['title'])
+                            <h3 class="card-row__item__title">{{ $item['title'] }}</h3>
+                        @endif
+                        @if ($item['link'])
+                            @include('partials.button', [
+                            'type' => $item['type'],
+                            'link' => $item['link'],
+                            'text' => $item['text'],
+                            'colour' => $item['btn_colour'],
+                            ])
+                        @endif
+                      </div>
+                  </div>
+              </div>
+          </div>
+      @endforeach
+   </div>
+</section>
