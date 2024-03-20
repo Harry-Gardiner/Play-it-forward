ffd467493ac4428399e33323bc48c7b69d49aa52
Harry Gardiner Lunar <harry@lunar.build>
2024-03-20 22:48:53 +0000

    Added swipe text back in

--- site/web/app/themes/sage/resources/views/blocks/timeline.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/timeline.blade.php
@@ -13,7 +13,7 @@
           @include('partials.title', [$title_style])
       @endif
       <p class="timeline__instructions timeline__instructions--desktop">Click & Drag</p>
-      {{-- <p class="timeline__instructions timeline__instructions--mobile">Swipe</p> --}}
+      <p class="timeline__instructions timeline__instructions--mobile">Swipe</p>
     </div>
   @endif
   <div class="timeline__cards-wrapper">
