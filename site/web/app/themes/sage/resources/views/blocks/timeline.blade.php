6b3b7448d23b12a03364a432b6b4a8c0f440fce9
Harry Gardiner Lunar <harry@lunar.build>
2024-03-20 22:02:20 +0000

    Styled mobile

--- site/web/app/themes/sage/resources/views/blocks/timeline.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/timeline.blade.php
@@ -1,15 +1,19 @@
-{{-- @php
-$cards = $fields['cards'];
-$theme = $fields['theme'] ? 'timeline--' . $fields['theme'] : '';
-@endphp --}}
+@php
+// $cards = $fields['cards'];
+// $theme = $fields['theme'] ? 'timeline--' . $fields['theme'] : '';
+// dump($title_style);
+@endphp
+
 
 @if ($cards && !empty($cards))
 <section class="section--timeline">
-  @if (isset($title))
+  @if (isset($title_style))
     <div class="timeline__text-wrapper">
-      <h2 class="timeline__title">{{ $title }}</h2>
+      @if ($title_style['title'])
+          @include('partials.title', [$title_style])
+      @endif
       <p class="timeline__instructions timeline__instructions--desktop">Click & Drag</p>
-      <p class="timeline__instructions timeline__instructions--mobile">Swipe</p>
+      {{-- <p class="timeline__instructions timeline__instructions--mobile">Swipe</p> --}}
     </div>
   @endif
   <div class="timeline__cards-wrapper">
@@ -33,57 +37,8 @@ $theme = $fields['theme'] ? 'timeline--' . $fields['theme'] : '';
           @endforeach
         </div>
       </div>
-</section>
-@endif
-
-
-{{-- @component('patterns.section.section', [
-'sectionClass' => 'section--timeline ' . $theme,
-"bg_class" => "",
-"attributes" => [
-"data-scroll-section" => 1
-]
-])
-
-@if(isset($title))
-<div class="timeline__text-wrapper">
-  <h2 class="timeline__title">{{ $fields['title'] }}</h2>
-
-  <p class="timeline__instructions timeline__instructions--desktop">Click & Drag</p>
-  <p class="timeline__instructions timeline__instructions--mobile">Swipe</p>
-
-</div>
-
-<div class="timeline__cards-wrapper">
-
-  @if (is_array($cards))
-
-  <div class="embla timeline__slider">
-    <div class="embla__buttons">
-      <button class="embla__button embla__button--prev" type="button" disabled="" tabindex="0">
-        <span class="visually-hidden">Previous slide</span>
-      </button>
-      <button class="embla__button embla__button--next" type="button" disabled="" tabindex="0">
-        <span class="visually-hidden">Next slide</span>
-      </button>
-    </div>
-
-    <div class="embla__viewport">
-      <div class="embla__container">
-
-        @foreach ($cards as $card)
-        <div class="embla__slide" tabindex="0">
-          @include('patterns.timeline-card.timeline-card', $card)
-        </div>
-        @endforeach
-
-      </div>
     </div>
   </div>
-  @endif
 
-</div>
-
+</section>
 @endif
-
-@endcomponent --}}
