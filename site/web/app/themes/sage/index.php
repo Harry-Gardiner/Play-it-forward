e5212c19be91fd8d1b14b7c2037f2ec7a13880c3
Harry Gardiner Lunar <harry@lunar.build>
2024-06-21 14:51:28 +0100

    Added google tag code snippet

--- site/web/app/themes/sage/index.php
+++ site/web/app/themes/sage/index.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113592678-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-113592678-1');
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
