716e25ec470eb2a919e79dfd88cd16f3e40bd1ab
Harry <gardiner_harry@yahoo.com>
2026-05-01 09:07:39 +0100

    Added conditional wrapper for publication title

--- site/web/app/themes/sage/resources/views/blocks/news-media.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/news-media.blade.php
@@ -1,8 +1,8 @@
 @php
     $background_colour = $background_colour ?? 'off-white';
-    $hasPublications   = !empty($publications);
-    $hasAnyContent     = !empty($combined_posts) || $hasPublications;
-    $showLoadMore      = ($has_more_blog ?? false) || ($has_more_news ?? false);
+    $hasPublications = !empty($publications);
+    $hasAnyContent = !empty($combined_posts) || $hasPublications;
+    $showLoadMore = ($has_more_blog ?? false) || ($has_more_news ?? false);
 @endphp
 
 <section class="news-media {{ $wrapper ?? '' }} {{ $spacing_size ?? '' }} bg--{{ $background_colour }} full-bleed">
@@ -54,13 +54,12 @@
                                 <div class="card__banner">
                                     <img src="@asset('images/bitOfEverything.png')" alt="">
                                 </div>
-                                <a href="{{ $pub['publication_file']['url'] }}" target="_blank" rel="noopener noreferrer">
+                                <a href="{{ $pub['publication_file']['url'] }}" target="_blank"
+                                    rel="noopener noreferrer">
                                     <div class="card__image">
                                         @if (!empty($pub['publication_image']))
-                                            <img
-                                                src="{{ $pub['publication_image']['sizes']['medium_large'] }}"
-                                                alt="{{ $pub['publication_image']['alt'] ?? $pub['publication_title'] }}"
-                                            >
+                                            <img src="{{ $pub['publication_image']['sizes']['medium_large'] }}"
+                                                alt="{{ $pub['publication_image']['alt'] ?? $pub['publication_title'] }}">
                                         @else
                                             <img src="{{ asset('images/placeholder.png') }}" alt="Publication cover">
                                         @endif
@@ -70,9 +69,11 @@
                                         @if (!empty($pub['publication_date']))
                                             <span class="card__date">{{ $pub['publication_date'] }}</span>
                                         @endif
-                                        <h3 class="card__title h4">
-                                            <span>{{ $pub['publication_title'] }}</span>
-                                        </h3>
+                                        @if ($pub['publication_title'])
+                                            <h3 class="card__title h4">
+                                                <span>{{ $pub['publication_title'] }}</span>
+                                            </h3>
+                                        @endif
                                     </div>
                                 </a>
                             </div>
@@ -90,20 +91,16 @@
                     <div class="spinner news-media__spinner">
                         <img src="{{ asset('images/football_loading.gif') }}" alt="Loading">
                     </div>
-                    <button
-                        class="button button--primary button--raspberry news-media__load-more"
+                    <button class="button button--primary button--raspberry news-media__load-more"
                         data-per-page="{{ $posts_per_type ?? 9 }}"
-                        data-page-blog="{{ ($has_more_blog ?? false) ? 1 : 0 }}"
-                        data-page-news="{{ ($has_more_news ?? false) ? 1 : 0 }}"
-                        data-has-more-blog="{{ ($has_more_blog ?? false) ? 'true' : 'false' }}"
-                        data-has-more-news="{{ ($has_more_news ?? false) ? 'true' : 'false' }}"
-                        hidden
-                    >
+                        data-page-blog="{{ $has_more_blog ?? false ? 1 : 0 }}"
+                        data-page-news="{{ $has_more_news ?? false ? 1 : 0 }}"
+                        data-has-more-blog="{{ $has_more_blog ?? false ? 'true' : 'false' }}"
+                        data-has-more-news="{{ $has_more_news ?? false ? 'true' : 'false' }}" hidden>
                         Load more
                     </button>
                 </div>
             @endif
-
         @else
             <p>No content available yet.</p>
         @endif
